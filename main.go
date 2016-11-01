316dfc053952258770a535ba695a64fe25cffc45
徐豪 <xuhao@wosai-inc.com>
2016-11-01 18:40:33 +0800

    add boot args

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"flag"
 	"net/http"
+	"strconv"
 
 	gomock "github.com/jacexh/gomock/lib"
 )
 
 func main() {
+	port := flag.Int("port", 8080, "the default listen port of gomock")
+	flag.Parse()
+
 	http.HandleFunc("/create", gomock.HandleCreate)
 	http.HandleFunc("/import", gomock.HandleImport)
 	http.HandleFunc("/export", gomock.HandleExport)
 	http.HandleFunc("/", gomock.HandleMock)
 
-	http.ListenAndServe(":8080", nil)
+	addr := strconv.FormatInt(int64(*port), 10)
+	gomock.Logger.Info("start gomock on port " + addr)
+	http.ListenAndServe(":"+addr, nil)
 }
